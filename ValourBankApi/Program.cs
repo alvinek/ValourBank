bf1d45abbff6d5cd346bc5c530616a11d06703ae
LqerC <LqerC@users.noreply.github.com>
2021-02-28 22:22:42 +0100

    Test

--- ValourBankApi/Program.cs
+++ ValourBankApi/Program.cs
@@ -10,27 +10,37 @@ namespace ConsoleApp1
 
     class Program
     {
+        private static string recieved_data { set; get; }
         static async Task Main(string[] args)
         {
             string login = "ACURASA", password = "123";
             password = Crypto.HashIt.Encrypt(password);
             Console.WriteLine(password);
-            await RequestAsync(login, password);
+            try
+            {
+                await RequestAsync(login, password);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
         }
 
 
 
         public static async Task RequestAsync(string login,string password)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://localhost:8080/?login={login},passwordhash={password}"));
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:8080/?login=" + login + ",passwordhash=" + password); request.ContentType = "text/html";
             HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
             using (Stream stream = response.GetResponseStream())
             {
                 using (StreamReader reader = new StreamReader(stream))
                 {
-                    Console.WriteLine(reader.ReadToEnd());
+                  Program.recieved_data  = reader.ReadToEnd();
                 }
             }
+
             response.Close();
         }
     }
