9bded33d4e33bdb0573abf3a1f2875aaf9271d55
LqerC <LqerC@users.noreply.github.com>
2021-02-28 20:25:51 +0100

    Base Request | Hardcode

--- ValourBankApi/Program.cs
+++ ValourBankApi/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.IO;
 using System.Threading.Tasks;
-
-namespace ValourBankApi
+using System.Security.Cryptography;
+using System.Text;
+using Crypto;
+namespace ConsoleApp1
 {
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string login = "ACURASA", password = "123";
+            password = Crypto.HashIt.Encrypt(password);
+            Console.WriteLine(password);
+            await RequestAsync(login, password);
+        }
+
+
+
+        public static async Task RequestAsync(string login,string password)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://localhost:8080/?login={login},passwordhash={password}"));
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
         }
     }
+    
 }
+
