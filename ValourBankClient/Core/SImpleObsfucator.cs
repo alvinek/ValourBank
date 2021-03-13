180e2f5a18b457c92b99c927297fcef839c46d49
alvinek <andrzejswit@gmail.com>
2021-03-14 00:08:59 +0100

    naprawiony

--- ValourBankClient/Core/SImpleObsfucator.cs
+++ ValourBankClient/Core/SImpleObsfucator.cs
@@ -7,10 +7,10 @@ namespace ValourBankApi
 {
     class SImpleObsfucator
     {
-        unsafe public static void PtrCreate(uint notptr = 32)
-        {
-            uint* ptr = (uint*)notptr;
-            Console.WriteLine((uint)&notptr);
-        }
+        //unsafe public static void PtrCreate(uint notptr = 32)
+        //{
+        //    uint* ptr = (uint*)notptr;
+        //    Console.WriteLine((uint)&notptr);
+        //}
     }
 }
